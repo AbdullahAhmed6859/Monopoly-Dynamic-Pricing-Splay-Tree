b73b3314d96b3cdc002c9f06e5aa73cf920dfe87
haris-hk <66428634+haris-hk@users.noreply.github.com>
2024-03-29 14:04:34 +0500

    cords

--- player.py
+++ player.py
@@ -1,11 +1,11 @@
 class Player:
-    def __init__(self, name, coors: tuple[int, int], hero, img):
+    def __init__(self, name, cords: tuple[int, int], hero, img):
         self.hero = hero  # pygame object which will be displayed on the screen
         self.name = name
         self.properties = []
         self.money = 1500
         self.position = 0
-        self.coors = coors
+        self.cords = cords
         self.in_jail = False
         self.jail_turns = 0
         self.get_out_of_jail_free = False
@@ -14,3 +14,5 @@ class Player:
     # for debugging
     def __str__(self):
         return f"Name: {self.name}, properties: {self.properties}"
+    
+    
