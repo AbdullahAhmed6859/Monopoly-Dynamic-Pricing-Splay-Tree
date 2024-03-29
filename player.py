0a8424d5cbce9f9a3e00fad647c7d21597cfbc3a
haris-hk <66428634+haris-hk@users.noreply.github.com>
2024-03-29 13:58:24 +0500

    made changes

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 class Player:
-    def __init__(self, name, coors: tuple[int, int], hero):
+    def __init__(self, name, coors: tuple[int, int], hero, img):
         self.hero = hero  # pygame object which will be displayed on the screen
         self.name = name
         self.properties = []
@@ -9,6 +9,7 @@ class Player:
         self.in_jail = False
         self.jail_turns = 0
         self.get_out_of_jail_free = False
+        self.img = img
 
     # for debugging
     def __str__(self):
